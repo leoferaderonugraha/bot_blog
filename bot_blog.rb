1dbc0d0ad938eb4c0adc39c48b5d080f61180900
leoferaderonugraha <leoferaderonugraha@gmail.com>
2019-07-24 21:54:12 +0700

    Fix updater

--- bot_blog.rb
+++ bot_blog.rb
@@ -1,4 +1,5 @@
 require 'open-uri'
+#require 'pry'
 
 #test output on heroku
 puts "Hello Rion"
@@ -14,39 +15,44 @@ open("https://www.proxy-list.download/api/v1/get?type=http"){|fp|
 proxies.flatten! #set the array to 1 dimension
 tmp = ''
 counter = 0
-time = Time.now.strftime("%I:%M").split(':')
+time = Time.now.strftime("%I:%M:%S").split(':')
 if time[0].to_i >= 11 #set reminder to 2 hours in 12 
-  dest = "#{time[0].to_i - 10}:#{time[1]}}"
+  dest = "#{time[0].to_i - 10}:#{time[1].to_i + 1}:#{time[2].to_i}".split(':')
 else
-  dest = "#{time[0].to_i + 2}:#{time[1]}"
+  dest = "#{time[0].to_i}:#{time[1].to_i + 1}:#{time[2].to_i}".split(':')
 end
 
 loop do
   begin
-    curtime = Time.now.strftime("%I:%M")
-    if curtime == dest || proxies.nil?  #if it's hit our 2 hours reminder or proxies is empty
+    curtime = Time.now.strftime("%I:%M:%S").split(':')
+    curtime = "#{curtime[0].to_i}:#{curtime[1].to_i}:#{curtime[2].to_i}".split(':')
+    if (curtime[0] == dest[0] && curtime[1] == dest[1] && curtime[2].to_i.between?(0,dest[2].to_i)) \
+        || proxies.nil?  #if it's hit our 2 hours reminder or proxies is empty
+      puts "Updating proxies..."
       proxies = Array.new
       open("https://www.proxy-list.download/api/v1/get?type=http"){|fp|
         proxies.append(fp.read.split("\r\n"))
       }
-    else
-      puts("#{curtime} - #{dest}")
-      proxy = proxies[rand(0...proxies.size)]
-      while proxy == tmp do #use another proxy if has been used before for fixing connection issue
-        proxy = proxies[rand(0...proxies.size)]
-      end
-      open(
-        'https://lfnugraha.blogspot.com/',
-        proxy: URI.parse("http://#{proxy}"),
-        :open_timeout => 3, #set timeout for connecting
-        :read_timeout => 3,
-        'User-Agent' => u_agent
-      ){|page|
-        counter += 1
-        puts "#{proxy} : #{page.status[1]} [#{counter}]"
-      }
-      tmp = proxy
+      proxies.flatten!
+      #binding.pry
+      #sleep 30
     end
+    puts("[#{curtime}] - [#{dest}]")
+    proxy = proxies[rand(0...proxies.size)]
+    while proxy == tmp do #use another proxy if has been used before for fixing connection issue
+      proxy = proxies[rand(0...proxies.size)]
+    end
+    open(
+      'https://lfnugraha.blogspot.com/',
+      proxy: URI.parse("http://#{proxy}"),
+      :open_timeout => 3, #set timeout for connecting
+      :read_timeout => 3,
+      'User-Agent' => u_agent
+    ){|page|
+      counter += 1
+      puts "#{proxy} : #{page.status[1]} [#{counter}]"
+    }
+    tmp = proxy
   rescue Timeout::Error
     proxies.delete(proxy)
     puts "Timeout on #{proxy}, deleted"
