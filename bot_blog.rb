801517cdfff283c8c4214516e9d9e56e8bc9e362
leoferaderonugraha <leoferaderonugraha@gmail.com>
2019-07-24 09:11:11 +0700

    adding new method to refresh every 2 hours

--- bot_blog.rb
+++ bot_blog.rb
@@ -3,30 +3,46 @@ require 'open-uri'
 u_agent = 'Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion'
 proxies = Array.new
 
-open("https://www.proxy-list.download/api/v1/get?type=https"){|fp|
+open("https://www.proxy-list.download/api/v1/get?type=http"){|fp|
   proxies.append(fp.read.split("\r\n"))
 }
 
 proxies.flatten! #set the array to 1 dimension
 tmp = ''
 counter = 0
+time = Time.now.strftime("%H:%M").split(':')
+if time[0].to_i >= 11
+  dest = "#{time[0].to_i - 10}:#{time[1]}}"
+else
+  dest = "#{time[0].to_i + 2}:#{time[1]}"
+end
+
 loop do
   begin
-    proxy = proxies[rand(0...proxies.size)]
-    while proxy == tmp do #use another proxy if has been used before for fixing connection issue
+    curtime = Time.now.strftime("%H:%M")
+    if curtime == dest
+      proxies = Array.new
+      open("https://www.proxy-list.download/api/v1/get?type=http"){|fp|
+        proxies.append(fp.read.split("\r\n"))
+      }
+    else
+      puts("#{curtime} - #{dest}")
       proxy = proxies[rand(0...proxies.size)]
+      while proxy == tmp do #use another proxy if has been used before for fixing connection issue
+        proxy = proxies[rand(0...proxies.size)]
+      end
+      open(
+        'https://lfnugraha.blogspot.com/',
+        proxy: URI.parse("http://#{proxy}"),
+        :open_timeout => 5, #set timeout for connecting
+        :read_timeout => 3,
+        'User-Agent' => u_agent
+      ){|page|
+        counter += 1
+        puts "#{proxy} : #{page.status[1]} [#{counter}]"
+      }
+      tmp = proxy
     end
-    open(
-      'https://lfnugraha.blogspot.com/',
-      proxy: URI.parse("http://#{proxy}"),
-      :open_timeout => 5, #set timeout for connecting
-      :read_timeout => 3,
-      'User-Agent' => u_agent
-    ){|page|
-      counter += 1
-      puts "#{proxy} : #{page.status[1]} [#{counter}]"
-    }
-    tmp = proxy
   rescue Timeout::Error
     puts "Timeout on #{proxy}"
   rescue Interrupt => e
